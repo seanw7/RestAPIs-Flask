908b8cbe548e9f02ae5641733aa10f85bdf1200d
seanw7 <seanw@protonmail.ch>
2017-05-19 09:38:46 -0700

    Added authentication to the API

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'testKey'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth  end point
 
 items = []
 
 class Item(Resource):
+	@jwt_required()
 	def get(self, name):
 		item = next(filter(lambda x: x['name'] == name, items), None)
 		return {'item': item}, 200 if item else 404
