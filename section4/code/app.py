53335de88932007d0b369784f7e05b9f2409d1bb
Sean Wilkie <seanw@protonmail.ch>
2017-05-20 15:34:09 -0700

    finished section 4, added parsing for json requests to make sure data is properly formatted

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -13,6 +13,13 @@ jwt = JWT(app, authenticate, identity) # /auth  end point
 items = []
 
 class Item(Resource):
+	parser = reqparse.RequestParser()
+	parser.add_argument('price',
+						type=float,
+						required=True,
+						help="This field cannot be left blank!"
+						)
+
 	@jwt_required()
 	def get(self, name):
 		item = next(filter(lambda x: x['name'] == name, items), None)
@@ -22,10 +29,30 @@ class Item(Resource):
 		if next(filter(lambda x: x['name'] == name, items), None) is not None:
 			return {'message': 'An item with name "{}" already exists'.format(name)}, 400 # 400 means bad request
 
-		data = request.get_json()
+		data = Item.parser.parse_args()
 		item = {'name': name, 'price': data['price']}
 		items.append(item)
 		return item, 201
+
+	def put(self, name):
+		data = Item.parser.parse_args()
+		item = next(filter(lambda x: x['name'] == name, items), None)
+		if item is None:
+			item = {'name': name, 'price': data['price']}
+			items.append(item)
+		else:
+			# dicts have the .update() method that is self-explanatory
+			item.update(data)
+		return item
+
+
+	def delete(self, name):
+		# This list(filter(lambda)) will find all of the items that DON'T match the supplied name and over write the list
+		# without the named item. items is the var name and its also being passed into the filter, will cause error.
+		global items
+		items = list(filter(lambda x: x['name'] != name, items))
+		return {'message': 'Item deleted'}
+
 
 class ItemList(Resource):
 	def get(self):
