889b7326431c5214e3a01501f178f0e0f7406dbf
Sean Wilkie <seanw@protonmail.ch>
2017-05-21 16:20:36 -0700

    Fixed functionality of the /items endpoint to return a list of all the items in the database

--- section5/code/item.py
+++ section5/code/item.py
@@ -14,12 +14,36 @@ class Item(Resource):
 	def find_by_name(cls, name):
 		connection = sqlite3.connect('data.db')
 		cursor = connection.cursor()
+
 		query = "SELECT * FROM items WHERE name=?"
 		result = cursor.execute(query, (name,))
+
 		row = result.fetchone()
 		connection.close()
 		if row:
 			return {"item": {"name": row[0], 'price': row[1]}}
+
+	@classmethod
+	def insert(cls, item):
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+
+		query = "INSERT INTO items VALUES (?, ?)"
+		cursor.execute(query, (item['name'], item['price']))
+
+		connection.commit()
+		connection.close()
+
+	@classmethod
+	def update(cls, item):
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+
+		query = "UPDATE items SET price=? WHERE name=?"
+		cursor.execute(query, (item['price'], item['name']))
+
+		connection.commit()
+		connection.close()
 
 	@jwt_required()
 	def get(self, name):
@@ -34,29 +58,31 @@ class Item(Resource):
 
 		# Makes sure the item json the user sends to us has a price field.
 		data = Item.parser.parse_args()
-
 		item = {'name': name, 'price': data['price']}
-
-		connection = sqlite3.connect('data.db')
-		cursor = connection.cursor()
-
-		query = "INSERT INTO items VALUES (?, ?)"
-		cursor.execute(query, (item['name'], item['price']))
-		connection.commit()
-		connection.close()
+		# Try and insert the POST data into the database.
+		try:
+			self.insert(item)
+		except:
+			return {"message": "An error occurred inserting the item."}, 500 # Internal Server Error
 
 		return item, 201
 
 	def put(self, name):
 		data = Item.parser.parse_args()
-		item = next(filter(lambda x: x['name'] == name, items), None)
+		item = self.find_by_name(name)
+		updated_item = {'name': name, 'price': data['price']}
+
 		if item is None:
-			item = {'name': name, 'price': data['price']}
-			items.append(item)
+			try:
+				self.insert(updated_item)
+			except:
+				return {"message": "An error occured inserting the item."}, 500
 		else:
-			# dicts have the .update() method that is self-explanatory
-			item.update(data)
-		return item
+			try:
+				self.update(updated_item)
+			except:
+				return {"message": "An error occurred updating the item."}, 500
+		return updated_item
 
 
 	def delete(self, name):
@@ -76,12 +102,13 @@ class ItemList(Resource):
 	def get(self):
 		connection = sqlite3.connect('data.db')
 		cursor = connection.cursor()
+
 		query = "SELECT * FROM items"
-		result = cursor.execute(query, (name,))
-		row = result.fetchall()
+		result = cursor.execute(query)
 
-		for row in cursor.execute(query):
-		    print(row)
-
+		items = []
+		for row in result:
+			items.append({"name": row[0], "price": row[1]})
 		connection.close()
+
 		return {'items': items}
